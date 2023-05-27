6bc2dcd6607b703d449b87c9dda783d7511024c4
Gulchachak <1gulchachak05@mail.ru>
2023-05-27 13:26:05 +0300

    seminar4

---
+++ Task 27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number) 
+{
+    int sum = 0; 
+    int[] arr = new int [number];
+    int n = arr.Length;
+    int i = 0;
+    while (i < n)
+    {
+        sum = sum+arr[i];
+        i++;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма цифр в числе = {sum}");
---
+++ Task 28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int multiplication(int number)
+{
+    int mult = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mult = multiplication(num);
+System.Console.WriteLine($"Произведение от 1 до {num} = {mult}");
+
